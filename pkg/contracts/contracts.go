67efb9a30089c73aad0b0b9dcd4a5d94c8d335d3
jgautier <jerome.gautier79@orange.fr>
2022-09-18 16:32:52 +0200

    Refactor services

--- pkg/contracts/contracts.go
+++ pkg/contracts/contracts.go
@@ -1,5 +1,29 @@
 package contracts
 
+import (
+	"github.com/gofiber/fiber"
+	"micro-fiber-test/pkg/commons"
+)
+
+func ConvertToInternalError(err error) commons.ApiError {
+	return commons.ApiError{
+		Code:    fiber.StatusInternalServerError,
+		Kind:    string(commons.ErrorTypeTechnical),
+		Message: err.Error(),
+	}
+}
+
 type IdResponse struct {
 	ID int64 `json:"id"`
+}
+
+type OrganizationListResponse struct {
+	Organizations []OrganizationResponse `json:"organizations"`
+}
+
+type OrganizationResponse struct {
+	Code   string `json:"code"`
+	Label  string `json:"label"`
+	Kind   string `json:"type"`
+	Status int    `json:"status"`
 }
