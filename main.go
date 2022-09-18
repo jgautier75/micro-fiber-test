d41f7990efc07237cb95f80aae46921d267c8ed5
jgautier <jerome.gautier79@orange.fr>
2022-09-18 13:55:35 +0200

    Add TLS support & yaml config management

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
 	"micro-fiber-test/pkg/dao/impl"
 	"micro-fiber-test/pkg/endpoints"
 	svcImpl "micro-fiber-test/pkg/service/impl"
-)
-
-const (
-	defaultTenantId = 1
-	rdbmsUrl        = "postgres://unicorn_user:magical_password@localhost:5432/rainbow_database"
 )
 
 func main() {
@@ -18,9 +17,19 @@ func main() {
 	orgDao := impl.OrgDao{}
 	orgSvc := svcImpl.NewOrgService(&orgDao)
 
+	// Load config file
+	var k = koanf.New(".")
+	err := k.Load(file.Provider("config/config.yaml"), yaml.Parser())
+	if err != nil {
+		fmt.Printf("Error loading confguration file [%v]", err)
+	}
+	targetPort := k.String("http.server.port")
+	defaultTenantId := k.Int64("app.tenant")
+	dbUrl := k.String("app.pgUrl")
+
 	app := fiber.New()
 
-	app.Post("/api/v1/organizations", endpoints.MakeOrgCreateEndpoint(rdbmsUrl, defaultTenantId, orgSvc))
+	app.Post("/api/v1/organizations", endpoints.MakeOrgCreateEndpoint(dbUrl, defaultTenantId, orgSvc))
 
-	app.Listen(":8080")
+	app.ListenTLS(":"+targetPort, "cert.pem", "key.pem")
 }
