0a9e783f1f07960505ecc0c7c071e7b85511b64d
jgautier <jerome.gautier79@orange.fr>
2022-09-21 22:05:31 +0200

    Transaction encapsulation

--- pkg/dao/api/orgDaoInterface.go
+++ pkg/dao/api/orgDaoInterface.go
@@ -1,6 +1,9 @@
 package api
 
-import "micro-fiber-test/pkg/model"
+import (
+	"github.com/jackc/pgx/v4"
+	"micro-fiber-test/pkg/model"
+)
 
 type OrgDaoInterface interface {
 	Create(cnxParams string, organization model.OrganizationInterface) (int64, error)
@@ -9,4 +12,5 @@ type OrgDaoInterface interface {
 	FindByCode(cnxParams string, code string) (model.OrganizationInterface, error)
 	FindAll(cnxParams string, tenantId int64) ([]model.OrganizationInterface, error)
 	ExistsByCode(cnxParams string, tenantId int64, code string) (bool, error)
+	CreateInTx(tx pgx.Tx, organization model.OrganizationInterface) (int64, error)
 }
--- pkg/dao/impl/sectorDao.go
+++ pkg/dao/impl/sectorDao.go
@@ -14,6 +14,13 @@ func NewSectorDao() api.SectorDaoInterface {
 	return &SectorDao{}
 }
 
+func (s SectorDao) CreateInTx(tx pgx.Tx, sector model.SectorInterface) (int64, error) {
+	var id int64
+	insertStmt := "insert into sectors(tenant_id,org_id,code,label,parent_id,has_parent,depth,status) values($1,$2,$3,$4,$5,$6,$7,$8) returning id"
+	errQuery := tx.QueryRow(context.Background(), insertStmt, sector.GetTenantId(), sector.GetOrgId(), sector.GetCode(), sector.GetLabel(), sector.GetParentId(), sector.GetHasParent(), sector.GetDepth(), sector.GetSectorStatus()).Scan(&id)
+	return id, errQuery
+}
+
 func (s SectorDao) Create(cnxParams string, sector model.SectorInterface) (int64, error) {
 	conn, err := pgx.Connect(context.Background(), cnxParams)
 	if err != nil {
