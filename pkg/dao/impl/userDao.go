dc7b8013f735b0b4c38e55c7747ae266bbe005c0
jgautier <jerome.gautier79@orange.fr>
2022-09-30 21:14:58 +0200

    Refactor user search and provide default error handler

--- pkg/dao/impl/userDao.go
+++ pkg/dao/impl/userDao.go
@@ -32,6 +32,33 @@ func (u UserDao) Create(cnxParams string, user model.UserInterface) (int64, erro
 	return id, errQuery
 }
 
+func (u UserDao) CountByCriteria(cnxParams string, criteria model.UserFilterCriteria) (int, error) {
+	conn, err := pgx.Connect(context.Background(), cnxParams)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close(context.Background())
+	if err != nil {
+		return 0, err
+	}
+	var fullQry strings.Builder
+	qryPrefix := "select count(1) from users where tenant_id=$1 and org_id=$2"
+	whereClause, vals := computeFindByCriteriaQuery(qryPrefix, criteria)
+	fullQry.WriteString(whereClause)
+	countRes, errCount := conn.Query(context.Background(), fullQry.String(), vals...)
+	if errCount != nil {
+		return 0, errCount
+	}
+	cnt := 0
+	for countRes.Next() {
+		err := countRes.Scan(&cnt)
+		if err != nil {
+			return 0, err
+		}
+	}
+	return cnt, nil
+}
+
 func (u UserDao) FindByCriteria(cnxParams string, criteria model.UserFilterCriteria) (model.UserSearchResult, error) {
 	conn, err := pgx.Connect(context.Background(), cnxParams)
 	searchResults := model.UserSearchResult{}
@@ -43,55 +70,24 @@ func (u UserDao) FindByCriteria(cnxParams string, criteria model.UserFilterCrite
 		return searchResults, err
 	}
 
-	var values []interface{}
-	var strBuf strings.Builder
-	var countBuf strings.Builder
-
-	values = append(values, criteria.TenantId)
-	values = append(values, criteria.OrgId)
-
-	countBuf.WriteString("select count(1) from users where tenant_id=$1 and org_id=$2")
-	strBuf.WriteString("select id,code,last_name,first_name,middle_name,login,email,status from users where tenant_id=$1 and org_id=$2")
-
-	inc := 3
-	if criteria.Login != "" {
-		values = append(values, criteria.Login)
-		countBuf.WriteString(fmt.Sprintf(" and login=%s", "%"+strconv.Itoa(inc)))
-		strBuf.WriteString(fmt.Sprintf(" and login=%s", "%"+strconv.Itoa(inc)))
-		inc = inc + 1
-	}
-	if criteria.Email != "" {
-		values = append(values, criteria.Email)
-		countBuf.WriteString(fmt.Sprintf(" and email=%s", "%"+strconv.Itoa(inc)))
-		strBuf.WriteString(fmt.Sprintf(" and email=%s", "%"+strconv.Itoa(inc)))
-		inc = inc + 1
-	}
-	if criteria.LastName != "" {
-		values = append(values, criteria.LastName)
-		countBuf.WriteString(fmt.Sprintf(" and last_name=%s", "%"+strconv.Itoa(inc)))
-		strBuf.WriteString(fmt.Sprintf(" and last_name=%s", "%"+strconv.Itoa(inc)))
-		inc = inc + 1
-	}
-	if criteria.FirstName != "" {
-		values = append(values, criteria.FirstName)
-		countBuf.WriteString(fmt.Sprintf(" and first_name=%s", "%"+strconv.Itoa(inc)))
-		strBuf.WriteString(fmt.Sprintf(" and first_name=%s", "%"+strconv.Itoa(inc)))
-	}
-
-	strBuf.WriteString(" order by  last_name,first_name asc")
+	var fullQry strings.Builder
+	qryPrefix := "select id,code,last_name,first_name,middle_name,login,email,status from users where tenant_id=$1 and org_id=$2"
+	whereClause, vals := computeFindByCriteriaQuery(qryPrefix, criteria)
+	fullQry.WriteString(whereClause)
+	fullQry.WriteString(" order by  last_name,first_name asc")
 
 	if criteria.Page > 1 {
 		startPg := (criteria.Page - 1) * criteria.RowsPerPage
-		strBuf.WriteString(" offset ")
-		strBuf.WriteString(strconv.Itoa(startPg))
+		fullQry.WriteString(" offset ")
+		fullQry.WriteString(strconv.Itoa(startPg))
 	}
 
-	strBuf.WriteString(" limit ")
-	strBuf.WriteString(strconv.Itoa(criteria.RowsPerPage))
+	fullQry.WriteString(" limit ")
+	fullQry.WriteString(strconv.Itoa(criteria.RowsPerPage))
 
-	query := strBuf.String()
+	query := fullQry.String()
 
-	rows, errQuery := conn.Query(context.Background(), query, values...)
+	rows, errQuery := conn.Query(context.Background(), query, vals...)
 	if errQuery != nil {
 		return searchResults, errQuery
 	}
@@ -124,20 +120,39 @@ func (u UserDao) FindByCriteria(cnxParams string, criteria model.UserFilterCrite
 
 	searchResults.Users = users
 
-	countQry := countBuf.String()
-	countRes, errCount := conn.Query(context.Background(), countQry, values...)
-	if errCount != nil {
-		return searchResults, errCount
-	}
-
-	cnt := 0
-	for countRes.Next() {
-		err := countRes.Scan(&cnt)
-		if err != nil {
-			return searchResults, err
-		}
-	}
-	searchResults.NbResults = cnt
-
 	return searchResults, nil
+}
+
+func computeFindByCriteriaQuery(qryPrefix string, criteria model.UserFilterCriteria) (string string, params []interface{}) {
+
+	var values []interface{}
+	var buf strings.Builder
+
+	values = append(values, criteria.TenantId)
+	values = append(values, criteria.OrgId)
+
+	buf.WriteString(qryPrefix)
+
+	inc := 3
+	if criteria.Login != "" {
+		values = append(values, criteria.Login)
+		buf.WriteString(fmt.Sprintf(" and login=%s", "%"+strconv.Itoa(inc)))
+		inc = inc + 1
+	}
+	if criteria.Email != "" {
+		values = append(values, criteria.Email)
+		buf.WriteString(fmt.Sprintf(" and email=%s", "%"+strconv.Itoa(inc)))
+		inc = inc + 1
+	}
+	if criteria.LastName != "" {
+		values = append(values, criteria.LastName)
+		buf.WriteString(fmt.Sprintf(" and last_name=%s", "%"+strconv.Itoa(inc)))
+		inc = inc + 1
+	}
+	if criteria.FirstName != "" {
+		values = append(values, criteria.FirstName)
+		buf.WriteString(fmt.Sprintf(" and first_name=%s", "%"+strconv.Itoa(inc)))
+	}
+	fullQry := buf.String()
+	return fullQry, values
 }
