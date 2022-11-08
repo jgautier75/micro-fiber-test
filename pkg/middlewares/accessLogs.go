cf27bf6bb49d44f65cdd4c8a31f9634c52d8b9a5
JGautier <j.gautier@lacroix.group>
2022-11-08 09:03:54 +0100

    Middlewares refactoring

--- pkg/middlewares/accessLogs.go
+++ pkg/middlewares/accessLogs.go
@@ -1,8 +1,7 @@
-package logging
+package middlewares
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"strings"
@@ -10,9 +9,7 @@ import (
 	"time"
 )
 
-const BsTxId = "bsTxId"
-
-func NewHttpFilterLogger(zapLogger *zap.Logger) fiber.Handler {
+func NewAccessLogger(zapLogger *zap.Logger) fiber.Handler {
 
 	var (
 		once       sync.Once
@@ -21,9 +18,6 @@ func NewHttpFilterLogger(zapLogger *zap.Logger) fiber.Handler {
 
 	return func(c *fiber.Ctx) (err error) {
 
-		var bsTxId string
-		bsTxId = uuid.New().String()
-		c.Locals(BsTxId, bsTxId)
 		defer func(zapLogger *zap.Logger) {
 			_ = zapLogger.Sync()
 		}(zapLogger)
@@ -38,7 +32,7 @@ func NewHttpFilterLogger(zapLogger *zap.Logger) fiber.Handler {
 
 		start = time.Now()
 
-		// Handle request, store err for logging
+		// Handle request, store err for middlewares
 		chainErr := c.Next()
 
 		stop = time.Now()
@@ -52,25 +46,12 @@ func NewHttpFilterLogger(zapLogger *zap.Logger) fiber.Handler {
 		}
 
 		var logReq = !strings.Contains(c.Path(), "assets") && !strings.Contains(c.Path(), ".html")
-		debugReq := false
-		reqHeaders := make([]string, 0)
-		for k, v := range c.GetReqHeaders() {
-			if strings.ToLower(k) == "x-log-debug" && v == "1" {
-				debugReq = true
-			}
-			reqHeaders = append(reqHeaders, k+"="+v)
-		}
-
-		if logReq && debugReq {
+		if logReq {
 			zapLogger.Info("HTTP",
 				zap.Field{Key: "method", Type: zapcore.StringType, String: c.Method()},
 				zap.Field{Key: "path", Type: zapcore.StringType, String: c.Path()},
 				zap.Field{Key: "ellapsed", Type: zapcore.Int64Type, Integer: duration.Milliseconds()},
 				zap.Field{Key: "http.response.status", Type: zapcore.Int64Type, Integer: int64(c.Response().StatusCode())},
-				zap.Field{Key: "http.request.headers", Type: zapcore.StringType, String: strings.Join(reqHeaders, "&")},
-				zap.Field{Key: "http.request.body", Type: zapcore.StringType, String: string(c.Body())},
-				zap.Field{Key: "http.response.body", Type: zapcore.StringType, String: string(c.Response().Body())},
-				zap.Field{Key: "bsTransactionId", Type: zapcore.StringType, String: bsTxId},
 			)
 		}
 		return nil
--- pkg/middlewares/httpFilter.go
+++ pkg/middlewares/httpFilter.go
@@ -1,7 +1,8 @@
-package logging
+package middlewares
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"strings"
@@ -9,7 +10,9 @@ import (
 	"time"
 )
 
-func NewAccessLogger(zapLogger *zap.Logger) fiber.Handler {
+const BsTxId = "bsTxId"
+
+func NewHttpFilterLogger(zapLogger *zap.Logger) fiber.Handler {
 
 	var (
 		once       sync.Once
@@ -18,6 +21,9 @@ func NewAccessLogger(zapLogger *zap.Logger) fiber.Handler {
 
 	return func(c *fiber.Ctx) (err error) {
 
+		var bsTxId string
+		bsTxId = uuid.New().String()
+		c.Locals(BsTxId, bsTxId)
 		defer func(zapLogger *zap.Logger) {
 			_ = zapLogger.Sync()
 		}(zapLogger)
@@ -32,7 +38,7 @@ func NewAccessLogger(zapLogger *zap.Logger) fiber.Handler {
 
 		start = time.Now()
 
-		// Handle request, store err for logging
+		// Handle request, store err for middlewares
 		chainErr := c.Next()
 
 		stop = time.Now()
@@ -46,12 +52,25 @@ func NewAccessLogger(zapLogger *zap.Logger) fiber.Handler {
 		}
 
 		var logReq = !strings.Contains(c.Path(), "assets") && !strings.Contains(c.Path(), ".html")
-		if logReq {
+		debugReq := false
+		reqHeaders := make([]string, 0)
+		for k, v := range c.GetReqHeaders() {
+			if strings.ToLower(k) == "x-log-debug" && v == "1" {
+				debugReq = true
+			}
+			reqHeaders = append(reqHeaders, k+"="+v)
+		}
+
+		if logReq && debugReq {
 			zapLogger.Info("HTTP",
 				zap.Field{Key: "method", Type: zapcore.StringType, String: c.Method()},
 				zap.Field{Key: "path", Type: zapcore.StringType, String: c.Path()},
 				zap.Field{Key: "ellapsed", Type: zapcore.Int64Type, Integer: duration.Milliseconds()},
 				zap.Field{Key: "http.response.status", Type: zapcore.Int64Type, Integer: int64(c.Response().StatusCode())},
+				zap.Field{Key: "http.request.headers", Type: zapcore.StringType, String: strings.Join(reqHeaders, "&")},
+				zap.Field{Key: "http.request.body", Type: zapcore.StringType, String: string(c.Body())},
+				zap.Field{Key: "http.response.body", Type: zapcore.StringType, String: string(c.Response().Body())},
+				zap.Field{Key: "bsTransactionId", Type: zapcore.StringType, String: bsTxId},
 			)
 		}
 		return nil
